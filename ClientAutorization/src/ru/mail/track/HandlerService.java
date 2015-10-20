b51c0b47819e11ecc4b3a166660af6f983fb98c6
arsdemal <arsdemal@gmail.com>
2015-10-20 16:48:21 +0300

    add command \find

--- ClientAutorization/src/ru/mail/track/HandlerService.java
+++ ClientAutorization/src/ru/mail/track/HandlerService.java
@@ -6,8 +6,6 @@ import java.util.Scanner;
 public class HandlerService {
 
     private User mainUser = null;
-    final static String FILE_USER_STORE = "C:\\Users\\Arsdemal\\Documents\\Projects\\JAVA\\tehnotrack\\ClientAutoriz" +
-            "ation\\resources\\userstore.txt";
 
     public int startHandler() throws IOException {
 
@@ -23,7 +21,7 @@ public class HandlerService {
                 mainUser.historyStore.addHistory(str);
             }
 
-            if (parts[0].charAt(0) == '\\') {
+            if ((parts[0].length() > 0) && parts[0].charAt(0) == '\\') {
                 switch (parts[0]) {
                     case "\\help":
                         System.out.println("You can use commands: user, history, exit");
@@ -37,7 +35,7 @@ public class HandlerService {
                                 System.out.println("Incorrect command");
                             } else {
                                 mainUser.setName(parts[1]);
-                                userStore.writeFile(FILE_USER_STORE);
+                                userStore.writeFile();
                             }
 
                         }
@@ -59,21 +57,34 @@ public class HandlerService {
                         }
                         break;
 
+                    case "\\find":
+                        if (mainUser == null) {
+                            System.out.println("You don't login");
+                        } else {
+                            if (parts.length != 2) {
+                                System.out.println("Incorrect");
+                            } else {
+                                mainUser.historyStore.findHistory(parts[1]);
+                            }
+                        }
+
+                        break;
+
                     case "\\login":
-                        userStore.readFile(FILE_USER_STORE);
+                        userStore.readFile();
                         AuthorizationService service = new AuthorizationService(userStore);
 
                         if (parts.length == 3) {
                             mainUser = service.login(parts[1],parts[2]);
                             if (mainUser != null) {
-                                System.out.println("Welcome");
+                                System.out.println("Welcome " + mainUser.getName());
                             }
                         } else {
                             if (parts.length == 1) {
                                 mainUser = service.createUser();
                                 if( mainUser != null) {
-                                    userStore.writeFile(FILE_USER_STORE);
-                                    System.out.println("Welcome");
+                                    userStore.writeFile();
+                                    System.out.println("Welcome " + mainUser.getName());
                                 }
                             }
                             else {
@@ -81,12 +92,15 @@ public class HandlerService {
                             }
                         }
                         break;
+                    /*case "\\exit":
+                        return 0
+                        break;*/
 
                     default:
                         System.out.println();
                         break;
                 }
-                if (parts[0].equals("\\exit")) {
+                if ((parts[0].length() > 0) && parts[0].equals("\\exit")) {
                     break;
                 }
             }
