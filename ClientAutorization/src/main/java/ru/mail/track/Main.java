129f9415d29dda748b2815059abc9af88ba06427
arsdemal <arsdemal@gmail.com>
2015-12-18 02:50:43 +0300

    simple logging

--- ClientAutorization/src/main/java/ru/mail/track/Main.java
+++ ClientAutorization/src/main/java/ru/mail/track/Main.java
@@ -1,5 +1,16 @@
 package ru.mail.track;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
+
+    static Logger log = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        log.info("info");
+        log.debug("debug");
+    }
+
 }
